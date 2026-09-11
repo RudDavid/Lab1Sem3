01f7fbfefcc3ad3751abf7966eb73ddd9e5fa292
RudDavid <satorugodze17@gmail.com>
2026-09-11 21:42:53 +0700

    first commit

---
+++ Lab1Sem3/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab1Sem3 {
+  public partial class Form1 : Form {
+    public Form1() {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e) {
+      Form2 form2 = new Form2();
+      this.Hide();                   
+      form2.ShowDialog();  
+      this.Show();        
+    }
+
+    private void button2_Click(object sender, EventArgs e) {
+      Form3 form3 = new Form3();
+      this.Hide();
+      form3.ShowDialog();
+      this.Show();
+    }
+
+    private void label1_Click(object sender, EventArgs e) {
+
+    }
+  }
+}
---
+++ Lab1Sem3/Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab1Sem3 {
+  public partial class Form2 : Form {
+    public Form2() {
+      InitializeComponent();
+    }
+
+    private void label3_Click(object sender, EventArgs e) {
+
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e) {
+
+    }
+
+    private void button1_Click(object sender, EventArgs e) {
+
+      double speed,
+        distance,
+        time,
+        timeForRepairs,
+        speedResult;
+
+      if (double.TryParse(textBox1.Text, out speed) && (speed > 0)) {
+        if (double.TryParse(textBox2.Text, out distance) && (distance > 0)) {
+          if (double.TryParse(textBox5.Text, out timeForRepairs) && (timeForRepairs > 0)) {
+
+            time = (distance / speed) + timeForRepairs;
+            speedResult = distance / time;
+
+            textBox6.Text = time.ToString();
+            textBox4.Text = speedResult.ToString();
+
+          } else {
+            MessageBox.Show("Incorrect time for repairs entry. ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+          }
+
+        } else {
+          MessageBox.Show("Incorrect distance entry. ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+      } else {
+        MessageBox.Show("Incorrect speed entry. ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+      }
+    }
+  }
+}
